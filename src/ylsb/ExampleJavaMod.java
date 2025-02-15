dbd8a6613b7c514dbe718f4f1ab6cee04dbf2b1d
yl222 <xxbhw@outlook.com>
2025-02-15 12:03:43 +0800

    yes

--- src/ylsb/ExampleJavaMod.java
+++ src/ylsb/ExampleJavaMod.java
@@ -1,11 +1,8 @@
-package example;
+package ylsb;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
